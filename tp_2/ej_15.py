de204558b30f2f6c8bed28dc19a84dc9cff13b78
Ale Cruz <hugoacsalt@gmail.com>
2024-03-02 18:45:37 -0300

    Realize el codigo pero tratar de corregirlo

--- tp_2/ej_15.py
+++ tp_2/ej_15.py
@@ -1,2 +1,20 @@
 '''Dada una lista de números no nulos que pertenecen a movimientos de dinero, en donde los números positivos corresponden a ingresos 
 y los números negativos corresponden a egresos. Se desea mostrar el total de ingresos, el total de egresos y el saldo'''
+n = int(input("Introduzca la cantidad de números para la lista: "))
+N = []
+tot_ingr = 0
+tot_egr = 0
+for i in range(n):
+    numero = int(input(f"Ingrese número {i+1} de la lista: "))
+    N.append(numero)
+print("La lista ingresada es: ", N)
+for monto in N:
+    if numero > 0:
+        tot_ingr += monto
+    else:
+        tot_egr += abs(monto)
+saldo = tot_egr-tot_ingr
+print("El total de ingresos es: ", tot_ingr)
+print("El total de egresos es: ", tot_egr)
+print("El saldo es de: ", saldo)
+#! Corregir código

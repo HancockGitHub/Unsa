b1897d0e15e29aa73da432e44b9c87f2993186d0
Ale Cruz <hugoacsalt@gmail.com>
2024-03-02 11:33:15 -0300

    Agregué el texto del ej 9 del tp2

---
+++ tp_2/ej_9.py
@@ -0,0 +1,4 @@
+'''La asociación de Vitivinicultores de Cafayate tiene como política fijar el precio inicial del kilo de uvas de acuerdo a su variedad: 
+Torrontes o Malbec. Además, clasifican su calidad en 1, 2 y 3 asignándole un precio diferenciado. 
+Realiza un algoritmo para determinar cuánto dinero recibirá un producto por una venta de una única variedad y calidad de uva, 
+considerando a información otorgada.'''
